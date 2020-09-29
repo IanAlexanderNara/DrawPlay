aeff24d377529c9182769ec1cb4c3b7df33e2d88
Andrew Taylor <astaylor@mines.edu>
2020-09-29 11:59:01 -0600

    Changed eye color to red

--- src/Cat.java
+++ src/Cat.java
@@ -30,7 +30,7 @@ public class Cat {
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.red); //changed eye color to red
 		x = catX + EYE_X + 1; //TODO
 		y = catY + EYE_Y + 1; //TODO
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
